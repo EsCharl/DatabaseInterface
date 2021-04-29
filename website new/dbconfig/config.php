adc2c66fe1e9912fceca5246e820e1a6693abed3
HolyKritGrenade <kythomas35@gmail.com>
2021-04-29 17:28:44 +0800

    Config file changed. This password is safe to give away because it only controls access to the DB (which we all have anyways)

--- website new/dbconfig/config.php
+++ website new/dbconfig/config.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","") OR die("cannot connect");
+$con = mysqli_connect("localhost","hcytt1mercury",'xE5{M{r]8x;o') OR die("cannot connect");
 mysqli_select_db($con,'entertainment');
 ?>
